498a0b503e9a4c4e69b6f3c51b21e3f039cff29b
arielbest44 <56075835+arielbest44@users.noreply.github.com>
2019-10-07 19:13:26 +0700

    Add files via upload

---
+++ 69Error.sh
@@ -0,0 +1,224 @@
+#usr/bin/bash
+clear
+python2 ariel.py
+sleep 5
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+
+echo $me" ║██████████╚╗"
+echo $me" ║██╔══╗█╔═╗█║"
+echo $me" ║██║╬╔╝█╚╗║█║"
+echo $me" ║██╚═╝█║█╚╝█║"
+echo $me" ╚╗█████████═╝"
+echo $pu"  ╚╗║╠╩╩╩╩╩╝"
+echo $pu"   ║║┈┈┈█▐█████▒.｡oO"
+echo $pu"   ║██╠╦╦╦╗"
+echo $pu"   ╚╗██████"
+echo $pu"    ╚════╝"
+
+echo ""
+sleep 2
+echo $ku"——————————————————————————————————————————————————————————"
+echo $pur"                   SELAMAT DATANG DI TOOLS 69"
+echo $i"                   AUTHOR   : Ariel Sandy Permana"
+echo $me"                   Team     : 69 M-Xad"
+echo $pur"                   WhatsApp  : 083100231366"
+echo $cy"                   Facebook : Ariel Permana"
+echo $i"                   Github   : https://github.com/arielbest44"
+echo $pur"——————————————————————————————————————————————————————————"
+
+
+echo $cy"            MENU" $ku" TOOLS" $i"69 :"
+echo ""
+echo $pur"           [1]" $i"TOOLS" $ku"MrD4N_v1"
+echo $pur"           [2]" $i"TOOLS" $ku"BAJINGANv6"
+echo $pur"           [3]" $i"TOOLS" $ku"TU4NBADUT"
+echo $pur"           [4]" $i"DARKFB" $ku"VIP"
+echo $pur"           [5]" $i"SPAM SMS" $ku"& CALL"
+echo $pur"           [6]" $i"DDOS" $ku"ATTACK (HAMMER)"
+echo $pur"           [7]" $i"LACAK" $ku"LOKASI"
+echo $pur"           [8]" $i"YAHOO" $ku"CLONING"
+echo $pur"           [9]" $i"LIHAT BOK3P" $ku"KHUSUS 18+"
+echo $pur"          [10]" $i"HACK" $ku"CCTV"
+echo $pur"          [11]" $i"M"$ku"B"$me"F"
+echo $pur"          [12]" $i"NUYUL" $ku"CAPING"
+echo $pur"          [13]" $i"TEMBAK" $ku"KUOTA"
+echo $pur"          [14]" $i"TOOLS" $ku"RUSMANA- ID"
+echo $pur"          [15]" $i"BUAT " $ku"PHISING"
+echo $pur"          [16]  $I"FB Target " $ku"FbTarget"
+echo $pur"          [17]  KE"$i"LU"$ku"AR"
+echo ""
+echo $ku" [SILAHKAN PILIH NOMOR]"
+read -p "@rootAriel ==>" 69 Team;@
+
+if [ $Ariel Sandy Permana = 1 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+pkg install figlet
+git clone https://github.com/MrD4N/Tools_v1
+cd Tools_v1
+sh ToolsMrD4N.sh
+fi
+
+
+if [ $Ariel Sandy Permana = 2 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/mrruxhangans/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $Ariel Sandy Permana = 3 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TU4NB4DUT.sh
+fi
+
+if [ $Ariel Sandy Permana = 4 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/m4rche3ll-cyber/dark-vpro
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $Ariel Sandy Permana = 5 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+sh troll.sh
+fi
+
+if [ $Ariel Sandy Permana = 6 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $Ariel Sandy Permana = 7 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $Ariel Sandy Permana = 8 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $Ariel Sandy Permana = 9 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/MrTamfanX/MrJav18V2
+cd MrJav18V2
+sh MrJav18V2.sh
+fi
+
+if [ $Ariel 69 = 10 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $Ariel 69 = 11 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $Ariel 69 = 12 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/widhisec/caping-bot
+cd caping-bot
+bash caping.sh
+fi
+
+if [ $Ariel 69 = 13 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/Bintang73/tembaktembakan
+cd tembaktembakan
+python dor.py
+fi
+
+if [ $Ariel 69 = 14 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh install.sh
+fi
+
+if [ $Ariel 69 = 15 ]
+then
+clear
+figlet -f slant "T U N G G U" | lolcat
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $Ariel 69 = 16 ]
+then
+clear
+figlet -f alamat "Ariel 69" | lolcat
+git clone https://github.com/sixtysix-Team/fbbrute
+cd fbbrute
+python2 force.py
+fi
+
+if [ $Ariel 69 = 17 ]
+then
+clear
+figlet -f slant "K E L U A R" | lolcat
+echo $pur"TERIMA KASIH PAKAI TOOLS INI YHA TOD"
+sleep 3
+echo $pu"GUNAKAN DENGAN BIJAK YHA SAYANGKU"
+sleep 3
+echo $i"JANGAN LUPA SUBSCRIBE CHANNEL : Ariel 69 Channel"
+sleep 3
+echo $ku"ASSALAMUALAIKUM"
+fi
